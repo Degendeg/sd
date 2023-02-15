1cec3f683f7d2955c2ae619b2808d7e29b2e2d40
Sebastian Degerman <sebastian.degerman@healthsolutions.se>
2023-02-15 11:27:11 +0100

    handle anchor, remove pathname and scrolltop on refresh

--- default.js
+++ default.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
 	const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
 	$('#age').text(getAge('1989-10-12'));
 	
-	$('#readmore').click(function () {
-      $('html, body').animate({
-        scrollTop: $("#about").offset().top
-      });
-	});
+	if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+	  window.scrollTo(0, 0);
+	  window.history.replaceState(null, null, window.location.pathname);
+	}
 });
