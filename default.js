6afd46f3e36fff409a2f083b39f085fdb0022941
Sebastian Degerman <sebastian.degerman@healthsolutions.se>
2023-02-15 11:19:42 +0100

    fix read more, fix css for avatar and native scrollbar

--- default.js
+++ default.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
 	const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
 	$('#age').text(getAge('1989-10-12'));
+	
+	$('#readmore').click(function () {
+      $('html, body').animate({
+        scrollTop: $("#about").offset().top
+      });
+	});
 });
