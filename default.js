6fbd04ac09aff245f8f078371317918cad346dba
Sebastian <sebastian.degerman@consid.se>
2024-01-10 15:11:17 +0100

    add CV, remove jQuery, update default.css & default.js, beautify HTML

--- default.js
+++ default.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', () => {
 	const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
-	$('#age').text(getAge('1989-10-12'));
-	
-	if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-	  window.scrollTo(0, 0);
-	  window.location.href = window.location.origin;
+	document.getElementById('age').textContent = getAge('1989-10-12');
+
+	const navigationType = performance.getEntriesByType('navigation')[0].type;
+	if (navigationType === 'reload') {
+		window.scrollTo(0, 0);
+		window.location.href = window.location.origin;
 	}
 });
